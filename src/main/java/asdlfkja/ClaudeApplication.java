e0a5f695fbf8fab297f1e8d60627d77cbe6a334b
Jessica Kerr <jessitron@gmail.com>
2018-03-07 14:58:29 +0000

    Initial commit from Atomist

---
+++ src/main/java/asdlfkja/ClaudeApplication.java
@@ -0,0 +1,12 @@
+package asdlfkja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaudeApplication.class, args);
+	}
+}
